a9393375c6bf69a6817c88601db1dac515b827b4
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-06-07 11:50:39 -0300

    Add files via upload

--- Lista de Compras/static/script.js
+++ Lista de Compras/static/script.js
@@ -1,28 +1,31 @@
 // Criando array da lista de compras
 var listaDeCompras = []
 
+// Variáveis para pegar os elementos do html
+var ulListaDeCompras = document.getElementById("listaDeCompras")
+
 function adicionarNaLista() {
+
+    console.log('Adicionando')
 
     // Variáveis para pegar os elementos do html
     var inputFazerCompra = document.getElementById("fazerCompra").value
-    var ulListaDeCompras = document.getElementById("listaDeCompras")
 
     // Validando se o input está vazio
     if (inputFazerCompra == "") {
         alert('Digite algum item para ser adicionado na lista de compras.')
-    } else if (listaDeCompras.includes(inputFazerCompra)) {
+    }
+
+    // Validando duplicidade (caso tente adicionar um item que ja existe)
+    else if (listaDeCompras.includes(inputFazerCompra)) {
         alert('Você já adicionou esse item na lista. Adicione outro item.')
-    } else {
+    }
+
+    // Condição caso tudo esteja em ordem
+    else {
+
         // Adicionando o valor do input dentro da lista de compras
         listaDeCompras.push(inputFazerCompra)
-
-        // Atualizando lista na tabela
-        ulListaDeCompras.innerHTML = '';
-
-        listaDeCompras.forEach(item => {
-            ulListaDeCompras.innerHTML += `<li>${item}</li>`;
-        });
-
         alert(`Você tem ${listaDeCompras.length} itens na lista`)
     }
 
@@ -31,10 +34,27 @@ function adicionarNaLista() {
 
 function removerDaLista(index) {
 
+    console.log('Removendo')
+
+    // Removendo e alertando qual item foi removido
+    alert(`Item ${listaDeCompras[index]} removido`)
+    listaDeCompras.splice(index, 1)
+
     atualizarListaNaTela()
 }
 
 function atualizarListaNaTela() {
+
+    console.log('Atualizando')
+
     // Limpando input
-    inputFazerCompra = ""
+    var inputFazerCompra = document.getElementById("fazerCompra")
+    inputFazerCompra.value = ""
+
+    // Atualizando lista na tabela
+    ulListaDeCompras.innerHTML = '';
+    listaDeCompras.forEach(item => {
+        ulListaDeCompras.innerHTML += `<li>${item}<button onclick="removerDaLista(${listaDeCompras.indexOf(item)})" class="btnRemover">Remover</button></li>`;
+    });
+
 }
